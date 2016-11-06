b0a6fe7297977a319cb7bc5c5ea7b16c35da92b9
making <makingx@gmail.com>
2016-11-06 23:13:38 +0900

    reactor version

--- src/main/java/am/ik/demo/WakatiKuromojiProcessorApplication.java
+++ src/main/java/am/ik/demo/WakatiKuromojiProcessorApplication.java
@@ -1,42 +1,36 @@
 package am.ik.demo;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
 
 import com.atilika.kuromoji.ipadic.Token;
 import com.atilika.kuromoji.ipadic.Tokenizer;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @EnableBinding(Processor.class)
 public class WakatiKuromojiProcessorApplication {
 
 	private final Tokenizer tokenizer = new Tokenizer();
-	private final MessageChannel output;
 	private static final Logger log = LoggerFactory
 			.getLogger(WakatiKuromojiProcessorApplication.class);
 
-	public WakatiKuromojiProcessorApplication(MessageChannel output) {
-		this.output = output;
-	}
-
-	@StreamListener(Processor.INPUT)
-	public void handle(String text) {
-		List<Token> tokens = tokenizer.tokenize(text);
-		log.info("Tokenized to {} tokens", tokens.size());
-		for (Token token : tokens) {
-			output.send(
-					MessageBuilder.withPayload(TokenConverter.convert(token)).build());
-		}
+	@StreamListener
+	@Output(Processor.OUTPUT)
+	public Flux<TokenBean> handle(@Input(Processor.INPUT) Flux<String> textStream) {
+		return textStream.log("stream.input")
+				.flatMap(text -> Flux.fromIterable(tokenizer.tokenize(text)))
+				.map(TokenConverter::convert).log("stream.output");
 	}
 
 	public static void main(String[] args) {
